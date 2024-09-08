e6f0c3fbe3cb2ea6be23e830259e638e11a1b592
Israel Bobadilla Segundo <danbobadilla9@hotmail.com>
2024-09-08 13:57:10 -0600

    - Terminando el backend 	-Es un proyecto muy sencillo, JPA un CRUD normal

--- backend-products/src/main/java/com/bobadilla/spring/backend/backend_products/repositories/ProductRepository.java
+++ backend-products/src/main/java/com/bobadilla/spring/backend/backend_products/repositories/ProductRepository.java
@@ -1,5 +1,14 @@
 package com.bobadilla.spring.backend.backend_products.repositories;
 
-public interface ProductRepository extends{
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.bobadilla.spring.backend.backend_products.entities.Product;
+
+// no se utiliza el / 
+@RepositoryRestResource( path = "products" )
+public interface ProductRepository extends CrudRepository<Product,Long> {
+
+
     
 }
